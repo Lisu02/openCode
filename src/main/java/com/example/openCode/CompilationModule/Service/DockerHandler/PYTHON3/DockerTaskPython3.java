381099bed8ac7e0ab68d633529d8e901dd972d78
Lisu02 <terminatorektt8000@gmail.com>
2024-11-20 00:24:36 +0100

    python task generation script [in progress]

--- src/main/java/com/example/openCode/CompilationModule/Service/DockerHandler/PYTHON3/DockerTaskPython3.java
+++ src/main/java/com/example/openCode/CompilationModule/Service/DockerHandler/PYTHON3/DockerTaskPython3.java
@@ -1,5 +1,7 @@
 package com.example.openCode.CompilationModule.Service.DockerHandler.PYTHON3;
 
+import com.example.openCode.CompilationModule.Model.Task.FunctionArgument;
+import com.example.openCode.CompilationModule.Model.Task.ReturnType;
 import com.example.openCode.CompilationModule.Model.Task.Task;
 import com.example.openCode.CompilationModule.Model.Task.TestTask.TestArgument;
 import com.example.openCode.CompilationModule.Model.Task.TestTask.TestTask;
@@ -132,6 +134,101 @@ public class DockerTaskPython3 implements DockerTaskLanguage {
         builder.append("        sys.exit(1)\n\n");
         builder.append("    user_file = sys.argv[1]\n");
         builder.append("    main(user_file)\n");
+    }
+
+    private void generatePythonTestScript222(Task task,StringBuilder builder) {
+        builder.append("import importlib.util\n");
+        builder.append("import argparse\n\n");
+        builder.append("FUNCTION_NAME = \"").append(task.getFunctionName()).append("\"\n\n");
+
+        builder.append("def load_module_from_file(file_path):\n");
+        builder.append("\tspec = importlib.util.spec_from_file_location(\"user_solution\", file_path)\n");
+        builder.append("\tuser_module = importlib.util.module_from_spec(spec)\n");
+        builder.append("\tspec.loader.exec_module(user_module)\n");
+        builder.append("\t user_module\n\n");
+
+        builder.append("def test(operation");
+        for (FunctionArgument functionArgument : task.getArgumentList()) {
+            builder.append(",").append(functionArgument.getName());
+        }
+        builder.append(", expected, overall, failed):\n");
+        builder.append("\ttry:\n");
+        builder.append("\t\tresult = operation(");
+        Iterator<FunctionArgument> funArgIterator = task.getArgumentList().iterator();
+        while (funArgIterator.hasNext()) {
+            builder.append(funArgIterator.next().getName());
+            if (funArgIterator.hasNext()) builder.append(", ");
+        }
+        builder.append(")\n");
+        builder.append("\t\tif result == expected:\n");
+        builder.append("\t\t\toverall[0] += 1\n");
+        builder.append("\t\telse:");
+        builder.append("\t\t\toverall[0] += 1\n");
+        builder.append("\t\t\tfailed[0] += 1\n");
+        builder.append("\t\t\tprint(\"Test failed: {operation.__name__}(");
+        funArgIterator = task.getArgumentList().iterator();
+        while (funArgIterator.hasNext()) {
+            builder.append("{").append(funArgIterator.next().getName()).append("}");
+            if (funArgIterator.hasNext()) builder.append(", ");
+        }
+        builder.append(") = {result}, expected {expected}\")\n");
+        builder.append("\texcept Exception as e:\n");
+        builder.append("\t\toverall[0] += 1\n");
+        builder.append("\t\tfailed[0] += 1\n");
+        builder.append("\t\tprint(\"Test failed with exception: {e}\")\n");
+
+        builder.append("def main():\n");
+        builder.append("\tparser = argparse.ArgumentParser()\n");
+        builder.append("\tparser.add_argument(\"solution_file\");\n");
+        builder.append("\targs = parser.parse_args()\n\n");
+
+        builder.append("\tuser_module = load_module_from_file(args.solution_file)\n\n");
+
+        builder.append("\toperation = getattr(user_module, FUNCTION_NAME)\n\n");
+
+        builder.append("\toverall = [0]\n");
+        builder.append("\tfailed = [0]\n");
+        builder.append("\ttest_cases = [\n");
+        for(TestTask testTask : task.getTestList()) {
+            Iterator<TestArgument> testArgIterator = testTask.getTestArguments().iterator();
+            while (testArgIterator.hasNext()) {
+                TestArgument testArgument = testArgIterator.next();
+                builder.append("\t\t");
+                //TODO: WYPISAĆ ARGUMENTY ZE WZGLĘDU NA TYP writeProperArgumentForTestCase
+                builder.append("\t\t").append(testArgIterator.next().getArgument());
+                if (testArgIterator.hasNext()) builder.append(", ");
+            }
+            builder.append("),\n");
+        }
+        builder.append("\t]\n\n");
+
+        builder.append("");
+
+
+
+    }
+
+    private void writeProperArgumentForTestCase(TestArgument testArgument, StringBuilder builder) {
+        switch (testArgument.getType()){
+            case CHAR -> builder.append("'").append(testArgument.getArgument()).append("'"); // 'A'
+            case STRING -> builder.append("\"").append(testArgument.getArgument()).append("\""); // "slowo"
+            case INTVECTOR -> builder.append("[").append(testArgument.getArgument()).append("]"); // [1,2,3]
+            case CHARVECTOR -> builder.append("[").append(testArgument.getArgument()).append("]"); // jest [a,b,c]
+            //a powinno byc ['a','b','c']?? //todo: sprawdzić w c czy charvector na pewno działa
+        }
+    }
+
+    private String getTypeToFunction(ReturnType returnType) {
+        return switch (returnType) {
+            case INT -> "int";
+            case FLOAT -> "float";
+            case DOUBLE -> "float";
+            case BOOLEAN -> "bool";
+            case CHAR -> "str";
+            case STRING -> "str";
+            case INTVECTOR -> "List[int]"; //from typing import List, Union dla laczenia typow
+            case CHARVECTOR -> "List[str]";
+        };
     }
 
 
